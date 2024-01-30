97fb033855ffdf8862207a7ebefc02e4716236e4
OzzySmith <158112361+OzzySmith@users.noreply.github.com>
2024-01-29 20:00:46 -0400

    ordem de precedência.py

---
+++ ordem de precedência.py
@@ -0,0 +1,21 @@
+# 5+2==7
+# 5-2==3
+# 5*2==10
+# 5**2==25
+# 5/2==2.5
+# 5//2==2
+# 5%2==1
+
+# função de potência (POW)
+# exemplo: pow(4,3) == 64
+
+# cálculo de raiz quadrada: calcular raiz quadrada é o mesmo que elevar a potência a metade
+# exemplo: 81**(1/2) == 9
+
+# ordem de precedência em Python
+# 1° = () 'parênteses'
+# 2° = ** 'potências'
+# 3° = *, /, //, % 'multiplicação, divisão, divisão inteira, resto da divisão'
+# 4° = +, - 'adição, subtração'
+
+
